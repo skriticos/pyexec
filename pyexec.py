7d5188517d6cbb63ff4e15a2b013a2154aec7235
Sebastian Bartos <seth.kriticos@gmail.com>
2013-08-31 20:48:32 +0200

    implemented run callback
    * create temporary directory
    * create file in directory
    * execute file in temporary directory
    * read stdout and stderr and write it to the output text edit
    * wait until done
    * write exit code to error code return widget

--- pyexec.py
+++ pyexec.py
@@ -23,7 +23,8 @@
 
 import os
 import sys
-import time
+import tempfile
+import subprocess
 
 from PySide.QtCore    import *
 from PySide.QtGui     import *
@@ -31,15 +32,58 @@ from PySide.QtUiTools import *
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def run():
+
+    root_window.txt_output.clear()
+    root_window.txt_exit_code.clear()
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+
+        execname = os.path.join(tmpdirname, 'run.py')
+        with open(execname, 'w') as f:
+            f.write(root_window.txt_input.toPlainText())
+
+        proc = subprocess.Popen(
+            ['/usr/bin/env', 'python3', execname],
+            cwd=tmpdirname,
+            shell=False,
+            close_fds=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+            )
+
+        while proc.poll() is None:
+
+            lines = proc.stdout.readlines()
+            proc.stdout.flush()
+            if lines:
+                txt = root_window.txt_output.toHtml()
+                for line in lines:
+                    txt += line.decode('utf-8') + '<br>'
+                root_window.txt_output.clear()
+                root_window.txt_output.setHtml(txt)
+
+            lines = proc.stderr.readlines()
+            proc.stderr.flush()
+            if lines:
+                txt = root_window.txt_output.toHtml()
+                for line in lines:
+                    txt += '<span style="color:red">{}</span>'.format(line.decode('utf-8')) + '<br>'
+                root_window.txt_output.clear()
+                root_window.txt_output.append(txt)
+
+        ret = proc.wait()
+        root_window.txt_exit_code.setText(str(ret))
+
 app = QApplication(sys.argv)
 
 loader = QUiLoader()
 uifile = QFile('root.ui')
 uifile.open(QFile.ReadOnly)
-
 root_window = loader.load(uifile)
-
 uifile.close()
+
+root_window.btn_run.clicked.connect(run)
 
 root_window.show()
 sys.exit(app.exec_())
